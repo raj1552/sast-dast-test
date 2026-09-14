ba2eaa962757f9e52e97097d4be3b365a68c2bce
Anuraj  Shrestha <anurajshrestha@m.m>
2026-09-14 11:33:32 +0545

    Fix Jest JSX parsing using next/jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+// next/jest automatically configures the SWC transform Next.js already
+// uses, so JSX/TS in test files works without needing a separate Babel setup
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
+
+module.exports = createJestConfig(customJestConfig);
